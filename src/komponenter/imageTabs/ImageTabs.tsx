9c0e9546623742e39805c8a6bf374ff80fa7aade
ossegosse <ossian.igelberg@gmail.com>
2024-04-05 14:26:27 +0200

    animation

--- src/komponenter/imageTabs/ImageTabs.tsx
+++ src/komponenter/imageTabs/ImageTabs.tsx
@@ -1,15 +1,54 @@
 import Tabs from "../tabs/Tabs";
-
+import "./ImageTabs.scss";
 
 const ImageTabs = () => {
   const tabs = [
-    { label: 'Flik 1', content: <img src="https://rb.gy/mi13km" />, },
-    { label: 'Flik 2', content: <img src="https://rb.gy/fu31fh" /> },
-    { label: 'Flik 3', content: <img src="https://rb.gy/2bovfl" /> },
+    {
+      label: "Flik 1",
+      content: (
+        <div>
+          <img src="https://rb.gy/mi13km" />{" "}
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati
+            delectus hic possimus dolor quibusdam! Nihil ex perspiciatis nemo.
+            Eum quo soluta similique iure eaque? Saepe reprehenderit reiciendis
+            quas blanditiis accusamus.
+          </p>
+        </div>
+      ),
+    },
+    {
+        label: "Flik 2",
+        content: (
+          <div>
+            <img src="https://rb.gy/fu31fh" />{" "}
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati
+              delectus hic possimus dolor quibusdam! Nihil ex perspiciatis nemo.
+              Eum quo soluta similique iure eaque? Saepe reprehenderit reiciendis
+              quas blanditiis accusamus.
+            </p>
+          </div>
+        ),
+      },
+      {
+        label: "Flik 3",
+        content: (
+          <div>
+            <img src="https://rb.gy/2bovfl" />{" "}
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati
+              delectus hic possimus dolor quibusdam! Nihil ex perspiciatis nemo.
+              Eum quo soluta similique iure eaque? Saepe reprehenderit reiciendis
+              quas blanditiis accusamus.
+            </p>
+          </div>
+        ),
+      },
   ];
 
   return (
-    <div>
+    <div className="ImageTabs">
       <h1>Tabs</h1>
       <Tabs tabs={tabs} />
     </div>
--- src/komponenter/tabs/Tabs.tsx
+++ src/komponenter/tabs/Tabs.tsx
@@ -1,31 +1,42 @@
 import {useState} from "react"
-import "./tabs.scss"
+import "./Tabs.scss"
 
-const Tabs = ({ tabs }) => {
-    const [activeTab, setActiveTab] = useState(0)
-
+function Tabs({ tabs }) {
+    const [activeTab, setActiveTab] = useState(null);
+  
     const handleTabClick = (index) => {
-        setActiveTab(index)
-    }
-
+      if (activeTab === index) {
+        setActiveTab(null);
+      } else {
+        setActiveTab(index);
+      }
+    };
+  
     return (
-        <div className="tabs">
-            <div className="tabButtons">
-                {tabs.map((tab, index) => (
-                    <button
-                    key={index}
-                    onClick={() => handleTabClick(index)}
-                    className={index === activeTab ? "active" : ""}
-                    >
-                    {tab.label}
-                    </button>
-                ))}
-            </div>
-            <div className={`tabContent ${activeTab === 1 ? 'fade-in' : 'fade-out'}`}>
-        {tabs[activeTab].content}
-      </div>
+        <div className="Tabs">
+          <div className="tabButtons">
+            {tabs.map((tab, index) => (
+              <button
+                key={index}
+                onClick={() => handleTabClick(index)}
+                className={index === activeTab ? "active" : ""}
+              >
+                {tab.label}
+              </button>
+            ))}
+          </div>
+          <div className="tab-content">
+            {tabs.map((tab, index) => (
+              <div
+                key={index}
+                className={index === activeTab ? "tab-pane active" : "tab-pane"}
+              >
+                {tab.content}
+              </div>
+            ))}
+          </div>
         </div>
-    )
-}
-
-export default Tabs
+      );
+    }
+    
+    export default Tabs;
